171b208421b914a4f6033f66c0bd7c95565be308
Dmitry Kiselev <sol.linux@gmail.com>
2017-01-27 01:04:49 +0200

    Set up scene, install BehaviorDesigner

---
+++ Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+    public class MoveToTarget : MonoBehaviour {
+        public Transform Target;
+        private NavMeshAgent agent;
+
+        protected void Awake() {
+            agent = GetComponent<NavMeshAgent>();
+        }
+	
+        protected void Update () {
+            agent.destination = Target.position;
+        }
+    }
+}
